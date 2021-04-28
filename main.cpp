6bf19c1f98909c23227f6f509a107177dd7eed4b
Iliass JABALI <iliassjabali@gmail.com>
2021-04-28 09:25:57 +0000

    °
    _

--- main.cpp
+++ main.cpp
@@ -2,15 +2,20 @@
 #include <bitset>
 using namespace std;
 
-string BinnaryMulti (int a, int b ){
-	bitset<8> B1 (a),  B2 (b);
-	bitset<8> sum = B1&= B2;
-
-	return sum.to_string();
+bitset<8> BinAddition (const bitset<8> a, const bitset<8> b ){
+	return 	a+b ; 
 }
 
+/*string BinnaryMulti (const int a, const int b ){
+	bitset<8> B1 (a),  B2 (b);
+	bitset<8> sum;
+	cout<< B1.to_string()<< endl;
+	cout<< B2.to_string()<<endl;
+	return B1.to_string();
+}*/
+
 int main() {
-	
-	cout << BinnaryMulti(20, 4 ) <<endl;
+	bitset<8> B1 (8),  B2 (16);
+	cout <<  B1+B2 <<endl;
 	return 0;
 }
