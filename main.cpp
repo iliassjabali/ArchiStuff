375ce93a32186ad56ce3fef3d2b8d34f1df34026
Iliass JABALI <iliassjabali@gmail.com>
2021-04-29 18:16:27 +0000

    WORKING BINARRY ADD AND MULTIPLY

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@
 #include <string>
 using namespace std;
 
-
-
-
-
-// this works for 
 bitset<8> operator + ( const bitset<8> num1, const bitset<8> num2 ){
+	//printf("We are adding the following two numbers %lu+%lu\n",num1.to_ulong(), num2.to_ulong()); 
+	//cout << "Binnary 1:\t" << num1 << "\t Binnary  2:\t" << num2 << endl;
 	bitset<8> sum, carry; 
 	// cout << "The size is " << num1.size()<< endl;
 	for (int i = 0; i< num1.size(); i++){
@@ -42,18 +39,41 @@ bitset<8> operator + ( const bitset<8> num1, const bitset<8> num2 ){
 }
 bitset<8> operator + ( const bitset<8> num1, const int num2 ){
 	//just adds a int to bit 
-	return num1 + bitset<8>(num1);
+	// using overloading 
+	return num1 + bitset<8>(num2);
 }
 
 bitset<8> operator * ( const bitset<8> num1, const bitset<8> num2 ){
-
+	// first step is to decalre the sum which should be all zeros 
+	bitset<8> sum (0), add = num1 ; 
+	for(  int i = 0; i < num2.size(); i++ ){
+		/*cout << "Loop number: " << i + 1;
+		if(num2[i] == 1) {
+			cout << "\tAdding" <<endl;
+		}
+		else  {
+			cout << "\tNot Adding" <<endl;
+		}
+		cout << "Sum:\t" << sum << "\nADD:\t" << add << endl;*/
+		//now we should go over each bit of the num2 and add the numbers only if it equales 1
+		if(num2[i] == 1){
+			sum = sum + add;
+			add  = (add << 1);
+		}
+		else{
+			//we will just have to shift regardless of the thingy 
+			add  = (add << 1);
+		}	
+	}
+	return sum;
 }
 
 int main() {
-	unsigned int N1 = 9, N2= 3;
-	bitset<8> B1 (N1),  B2 (N2), carry; 
-	cout<< "Binnary  1:\t" << B1<< endl<< "Binnary  2:\t" << B2<< endl;
-	bitset<8>  tmp = B1 + B2;
-	cout<<" The function will give us" <<endl<< tmp <<endl; //no neeed
+	unsigned int N1 = 96, N2= 2;
+	bitset<8> B1 (N1),  B2 (N2);
+	printf("Multiplaying %d*%d\n", N1, N2);
+	cout << "Binnary 1:\t" << B1 << "\t Binnary  2:\t" << B2 << endl;
+	bitset<8>  tmp = B1 * B2;
+	cout<<" The function will give us" <<endl<< tmp  << "\tWhich is\t" << tmp.to_ulong ()<<endl; //no neeed
 	return 0;
 }
